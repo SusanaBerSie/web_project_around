8064fdf7397c819490b6da34c6f329cc781aabac
SusanaBS <susana.bersie33@gmail.com>
2025-03-14 20:02:47 -0500

    arreglar desplazamiento lateral en mediaquery

--- FormValidator.js
+++ FormValidator.js
@@ -1,16 +1,3 @@
-/* Crea la clase FormValidator, que establece la configuración para validar los campos
-del formulario de acuerdo con los siguientes requisitos:
-
-- Tu constructor tiene dos parámetros. El primer parámetro es un objeto de configuración
-que almacena los selectores y las clases del formulario, y el segundo toma un elemento
-del formulario a validar.
-- Tiene métodos privados para procesar el formulario, que incluyen:
-comprobar la validez del campo, cambiar el estado del botón Submit, y agregar
-todos los controladores necesarios.
-- Tiene un método público enableValidation(), que activa la validación del formulario.
-- Crea una instancia de la clase FormValidator para cada formulario que deba ser validado.
- */
-
 class FormValidator {
   constructor(
     {
@@ -56,20 +43,13 @@ class FormValidator {
     });
   };
 
-  showInputError = (inputElement, errorMessage) => {
+  _showInputError = (inputElement, errorMessage) => {
     const inputError = this._formElement.querySelector(
       `.${inputElement.id}-error`
     );
-    console.log("Input ID:", inputElement.id);
-    console.log("Looking for error element:", `.${inputElement.id}-error`);
-    console.log("Found error element:", inputError);
-    console.log("Error message:", errorMessage);
-    console.log("error class to add:", this._errorClass);
     inputElement.classList.add(this._inputErrorClass);
     inputError.textContent = errorMessage;
-    console.log("setting error texto to:", errorMessage);
     inputError.classList.add(this._errorClass);
-    console.log("Error element after class added:", inputError.outerHTML);
   };
 
   hideInputError = (inputElement) => {
@@ -94,9 +74,9 @@ class FormValidator {
 
   _checkInputValidity = (inputElement) => {
     if (!inputElement.validity.valid) {
-      this.showInputError(inputElement, inputElement.validationMessage);
+      this._showInputError(inputElement, inputElement.validationMessage);
     } else {
-      this.hideInputError(inputElement);
+      this._hideInputError(inputElement);
     }
   };
 }
